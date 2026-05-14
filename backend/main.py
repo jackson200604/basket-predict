1beb50d4ba03ab5a95a66a972a4243bd1897e6b8
jackson200604 <zongojairus06@gmail.com>
2026-05-14 01:51:35 +0000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,9 +18,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins  = ["*"],
-    allow_methods  = ["*"],
-    allow_headers  = ["*"],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
